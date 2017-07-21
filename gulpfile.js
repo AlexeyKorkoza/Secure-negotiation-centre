cae48ea5bfe0b2f77f251df8bda315cf2162323d
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-07-21 11:13:47 +0300

    Change structure of directory, change several styles, change gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,54 +10,81 @@ const csslint = require('gulp-csslint');
 const connect = require('gulp-connect');
 const sass = require('gulp-sass');
 const jscpd = require('gulp-jscpd');
+const rename = require('gulp-rename');
+const notify = require('gulp-notify');
+const del = require('del');
+const cssnano = require('gulp-cssnano')
 
 gulp.task('connect', function() {
   connect.server({
-    root: '',
+    root: 'app',
     livereload: true
   });
 });
 
 gulp.task('sass', () => {
-  gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
+  return gulp.src('app/sass/**/*.scss')
+    .pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+    .pipe(concat('main.css'))
+    .pipe(rename({suffix: '.min', prefix : ''}))
+    .pipe(gulp.dest('app/css'))
     .pipe(connect.reload());
 });
 
 gulp.task('scripts', () => {
-  return gulp.src('js/*.js')
+  return gulp.src('app/js/*.js')
     .on('error', console.log)
-    .pipe(concat('build.js'))
+    .pipe(concat('scripts.js'))
+    .pipe(rename({suffix: '.min', prefix : ''}))
     .pipe(uglify())
     .pipe(jscpd())
-    .pipe(gulp.dest('./build/'))
+    .pipe(gulp.dest('app/js'))
     .pipe(connect.reload());
 });
 
-gulp.task('css', () => {
-  return gulp.src(['node_modules/bootstrap-grid/dist/grid.css','css/*.css'])
-    .on('error', console.log)
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(cssmin())
-    .pipe(csslint())
-    .pipe(concat('build.css'))
-    .pipe(gulp.dest('./build/'))
-    .pipe(connect.reload());
+gulp.task('css-libs', () => {
+    return gulp.src(['app/libs/grid.min.css'])
+        .pipe(concat('libs.css'))
+        .pipe(rename({suffix: '.min', prefix : ''}))
+        .pipe(gulp.dest('app/css'));
 });
 
 gulp.task('html', () => {
-  return gulp.src('index.html')
+  return gulp.src('app/index.html')
     .pipe(connect.reload());
 });
 
-gulp.task('watch', () => {
-    gulp.watch('sass/*', ['sass']);
-    gulp.watch('css/*', ['css']);
-    gulp.watch('index.html', ['html']);
+gulp.task('watch', ['css-libs', 'scripts'], () => {
+    gulp.watch('app/sass/*', ['sass']);
+    gulp.watch('app/js/*', ['scripts']);
+    gulp.watch('app/index.html', ['html']);
 });
 
-gulp.task('default', ['connect', 'html', 'css', 'sass', 'watch']);
+gulp.task('build', ['clean', 'sass', 'scripts'], () => {
+
+    var buildCss = gulp.src([
+        'app/css/*'
+    ])
+    .pipe(cssnano())    
+    .pipe(gulp.dest('dist/css'))
+
+    var buildFonts = gulp.src('app/fonts/*')
+    .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('app/js/*.js')
+    .pipe(gulp.dest('dist/js'))
+
+    var buildHtml = gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('clean', () => {
+    return del.sync('dist');
+});
+
+gulp.task('clear', () => {
+    return cache.clearAll();
+})
+
+gulp.task('default', ['connect', 'watch']);
